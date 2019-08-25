209de2d84b6c757d9b00d2d40f67911f5a050ad4
Mac <mac@Macs-MacBook.hotspot>
2019-08-25 20:24:55 +0300

    texteditor

--- src/App.js
+++ src/App.js
@@ -8,14 +8,25 @@ const styles = {
 };
 
 class App extends Component {
+  state = {
+    color: "black",
+    bold: false,
+    italic: false,
+    underline: false
+  };
+
   render() {
     let styleNames = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
-    let stylingBoxes = styleNames.map(style => {
+    let stylingBoxes = styleNames.map(x => {
       return (
-        <button style={styles[style]} key={style}>
-          {style}
+        <button
+          style={styles[x]}
+          key={x}
+          onClick={() => this.setState({ [x]: !this.state[x] })}
+        >
+          {x}
         </button>
       );
     });
@@ -25,6 +36,7 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.setState({ color: color })}
         />
       );
     });
@@ -32,7 +44,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="my-3">{stylingBoxes}</div>
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.color,
+            fontWeight: this.state.bold ? "bold" : "",
+            fontStyle: this.state.italic ? "italic" : "",
+            textDecorationLine: this.state.underline ? "underline" : ""
+          }}
+        />
         <div className="my-3">{colorBoxes}</div>
       </div>
     );
